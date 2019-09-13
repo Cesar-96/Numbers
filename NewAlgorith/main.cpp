b3e6456e45559cae3d50ef5aca06b54eb0807cd9
Pablo Yucra <cesar.yucra12@gmail.com>
2019-09-13 05:52:12 -0500

    last changes

--- NewAlgorith/main.cpp
+++ NewAlgorith/main.cpp
@@ -1,53 +1,56 @@
-
 #include<iostream>
 using namespace std;
 
-
-int numerosPerfectos(int n)
+//Ingresa numero y ver si es perfecto (for)
+/*int main()
 {
+    int num ,div=0,i;
+    cout<<"ingresa el num"<<endl;
+    cin>>num;
 
-    int a,b,c;
-    while(c<100)
-    {
-        cout<<c<<endl;
-        c=a+b;
-        a=b; b=c;
-    }
-
-
+    for (i=1; i< num;i++)
+        {
+            if(num%i == 0)
+                div += i;
+        }
+    if (num == div)
+        cout<<num <<" es perfecto"<<endl;
+    else
+        cout<<num <<" no es perfecto"<<endl;
+    return 0;
 }
-int numerosPrimos()
+*/
+
+
+//Ingresa numero e indica si es perfecto (while)
+/*int main()
 {
-  int n=0,c=0,c2=0,res=0,nc=0;
-  cout<<"Introduce el limite de numeros: "; cin>>n;
-  for(c=1;c<=n;c++)
-  {
-    for(c2=1;c2<=c;c2++)
+    int num,
+    div=0,
+    s=1;
+    cout<<"ingresa el numero"<<endl;
+    cin>>num;
+    while (s<num)
     {
-      res=c%c2;
-      if(res==0)
-      {
-        nc=nc+1;
-      }
+        if (num%s==0)
+            div += s;
+        s++;
     }
-    if(nc==2)
-    {
-      cout<<" "<<c;
-    }
-    nc=0;
-  }
+    if (num == div)
+        cout<<num<<" ,es perfecto"<<endl;
+    else
+        cout<<num<<" , no es perfecto"<<endl;
+    return 0;
 }
-//int main()
-//{   cout<<"numeros perfectos son:"<< endl;
-//    int a=0,b=1,c=1;
-//    int n;
-//    cout<<"ingresa n: "<<endl;
-//    cin>>n;
-//    int numerosPerfectos(n);
+*/
 
-//}
 
-int main(){
+
+
+//Programa que recibe un numero y dice si es primo o no (for)
+
+/*int main()
+{
    int a=0,i,n;
          cout<<"Ingrese numero"<<endl;
          cin>>n;
@@ -58,8 +61,83 @@ int main(){
          }
          if(a!=2){
               cout<<"No es Primo";
-            }else{
+            }else
+            {
                 cout<<"Si es Primo";
          }
     return 0;
 }
+*/
+
+//ingresa numero e indica si es primo (while)
+/*int main() {
+int divisor = 1, divisores = 0, num = 0;
+cout<<"Ingrese numero: ";
+cin>>num;
+do
+{
+if(num % divisor == 0){
+divisores++;
+}
+divisor++;
+}while(divisor <= num);
+if(divisores == 2)
+{
+cout<<"El numero "<<num<<" es PRIMO.";
+}else{
+cout<<"El numero "<<num<<" NO es PRIMO.";
+}
+return 0;
+}*/
+
+
+
+//Imprime los numeros primos hasta el 100(for)
+/*int main()
+{
+  int num=100,a=0,b=0,res=0,nc=0;
+
+  cout<<"Introduce el limite de numeros: "<<"1";
+  for(a=1;a<=num;a++)
+  {
+    for(b=1;b<=a;b++)
+    {
+      res=a%b;
+      if(res==0)
+      {
+        nc=nc+1;
+      }
+    }
+    if(nc==2)
+    {
+      cout<<" "<<a;
+    }
+    nc=0;
+  }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+int main()
+    {
+        int divisor , num=100 ,s=0,i=1;
+        while (divisor <= num)
+        {
+            if (num%divisor==0)
+                s++;
+                cout<<s;
+            divisor++;
+            cout<<s;
+        }
+    }
+*/
